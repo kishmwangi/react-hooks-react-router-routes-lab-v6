f482dcc781dbd3f1c42e87a2b0636fafd582a65d
kishmwangi <kishmwangi31@gmail.com>
2024-05-02 05:47:33 +0300

    router routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,29 @@
+import Actors from "./pages/Actors";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Directors from "./pages/Directors";
+import ErrorPage from "./pages/ErrorPage";
 
 
 const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  }
    
   ];
 
